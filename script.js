9ad92046241976da7e55691aefca441c7f8cc47a
FlorenciaCha <163207162+FlorenciaCha@users.noreply.github.com>
2024-05-22 13:39:45 -0300

    Enlace con pagina Eventos script.js
    Se agrega el enlace para que se redirija a la pagina eventos

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
  document.getElementById('contacto-btn').addEventListener('click', function() {
     window.location.href = './contacto.html';
 });
+
+
+ /* Agrega funcionalidad al boton Ver Mas en la seccion de eventos para redirigir a la pagina de eventos*/
+ document.getElementById('btn-ver-eventos').addEventListener('click', function() {
+    window.location.href = './eventos.html';
+});
