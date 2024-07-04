f514027856ee5e7e5f3b96067f0f252d4462d2c3
Adrian R <109531222+adru75@users.noreply.github.com>
2024-07-04 11:40:09 -0300

    agregados CRUD de bebidas y platos

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -80,7 +80,7 @@ const updateMenu = (req, res)=>{
         //Si todo sale bien
         res.json({mensaje: "Menu actualizado"});
    })
-}
+};
 
 //6- deleteMenu
 const deleteMenu = (req, res)=>{
@@ -97,15 +97,193 @@ const deleteMenu = (req, res)=>{
         //Si todo sale bien
         res.json({mensaje: "Menu eliminado"});
     })
+};
+
+//7- createPlato
+const createPlato = (req, res)=>{
+    //Desestructuramos la reques
+    const {nombre, descripcion} = req.body;
+
+    // crear la consulta
+    const sql = 'INSERT INTO platos (nombre, descripcion) VALUES (?, ?)';
+
+    // Pasar consulta
+    db.query(sql, [nombre, descripcion], (error, result)=>{
+         //Si sucede un error
+         if(error){throw error};
+         //Si todo sale bien
+         res.json({mensaje: "Plato creado"});
+    })
 }
 
+//8- createBebida
+const createBebida = (req, res)=>{
+    //Desestructuramos la reques
+    const {nombre, descripcion} = req.body;
+
+    // crear la consulta
+    const sql = 'INSERT INTO bebidas (nombre, descripcion) VALUES (?, ?)';
+
+    // Pasar consulta
+    db.query(sql, [nombre, descripcion], (error, result)=>{
+         //Si sucede un error
+         if(error){throw error};
+         //Si todo sale bien
+         res.json({mensaje: "Bebida creada"});
+    })
+}
+
+//9- getAllBebidas
+const getAllBebidas = (req, res)=>{
+    //Consulta para traer todos los menus
+    const sql = "SELECT * FROM menus_db.bebidas;";
+
+    //Enviar la consulta a la base de datos
+    db.query(sql, (error, results)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json(results);
+    });
+};
+
+//10- getAllPlatos
+const getAllPlatos = (req, res)=>{
+    //Consulta para traer todos los menus
+    const sql = "SELECT * FROM menus_db.platos;";
+
+    //Enviar la consulta a la base de datos
+    db.query(sql, (error, results)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json(results);
+    });
+};
+
+//11- getBebidaById
+const getBebidaById = (req,res)=>{
+    // Obtener id solicitado
+    //Notacion de desestructuracion {id}
+    const {id} = req.params;
+
+    // consulta a la bbdd
+    const sql = 'SELECT * FROM bebidas WHERE id = ?'
+
+    // Enviar la consulta
+    db.query(sql, [id], (error, result)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json(result);
+    })
+};
+//12- getPlatoById
+const getPlatoById = (req,res)=>{
+    // Obtener id solicitado
+    //Notacion de desestructuracion {id}
+    const {id} = req.params;
+
+    // consulta a la bbdd
+    const sql = 'SELECT * FROM platos WHERE id = ?'
+
+    // Enviar la consulta
+    db.query(sql, [id], (error, result)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json(result);
+    })
+};
+
+//13- updatePlato
+const updatePlato = (req, res)=>{
+    //Desestructurar la peticion
+    const {id} = req.params;
+    const {nombre, descripcion} = req.body;
+
+    // Consulta con marcadores 
+    const sql = 'UPDATE platos SET nombre = ?, descripcion = ?  WHERE id = ?';
+
+    // Pasar la consulta
+    db.query(sql, [nombre, descripcion, id], (error, result)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json({mensaje: "Plato actualizado"});
+   })
+};
+
+//14- updateBebida
+const updateBebida = (req, res)=>{
+    //Desestructurar la peticion
+    const {id} = req.params;
+    const {nombre, descripcion} = req.body;
+
+    // Consulta con marcadores 
+    const sql = 'UPDATE bebidas SET nombre = ?, descripcion = ?  WHERE id = ?';
+
+    // Pasar la consulta
+    db.query(sql, [nombre, descripcion, id], (error, result)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json({mensaje: "Bebida actualizada"});
+   })
+};
+
+
+//15- deletePlato
+const deletePlato = (req, res)=>{
+    //desestructuracion 
+    const {id} = req.params;
+
+    // Consulta sql
+    const sql = 'DELETE FROM platos WHERE id = ?';
+
+    // Pasamos la consulta a la base de datos
+    db.query(sql, [id], (error, result)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json({mensaje: "Plato eliminado:"+id});
+    })
+};
+
+//16- deleteBebida
+const deleteBebida = (req, res)=>{
+    //desestructuracion 
+    const {id} = req.params;
+
+    // Consulta sql
+    const sql = 'DELETE FROM bebidas WHERE id = ?';
+
+    // Pasamos la consulta a la base de datos
+    db.query(sql, [id], (error, result)=>{
+        //Si sucede un error
+        if(error){throw error};
+        //Si todo sale bien
+        res.json({mensaje: "Bebida eliminada:"+id});
+    })
+};
 // Exportamos el modulo
 module.exports = {
     getAllMenus,
+    getAllBebidas,
+    getAllPlatos,
     getMenuById,
+    getPlatoById,
+    getBebidaById,
     createMenu,
+    createPlato,
+    createBebida,
     updateMenu,
-    deleteMenu 
+    updatePlato,
+    updateBebida,
+    deleteMenu,
+    deletePlato,
+    deleteBebida 
+ 
 };
 
 
--- routes/menusRouter.js
+++ routes/menusRouter.js
@@ -10,20 +10,50 @@ const router = express.Router();
 const menuController = require("../controllers/menuController");
 
 //4- Plantear las solicitudes GET, POST, PUT, DELETE
-// Ruta del listado general
+// Ruta del listado menus
 router.get("/list", menuController.getAllMenus);
+
+// Ruta del listado bebidas
+router.get("/listBebidas", menuController.getAllBebidas);
+
+// Ruta del listado platos
+router.get("/listPlatos", menuController.getAllPlatos);
 
 // Ruta para consultas parametricas
 router.get("/:id", menuController.getMenuById);
 
+// Ruta para consultas parametricas
+router.get("/bebida/:id", menuController.getBebidaById);
+
+// Ruta para consultas parametricas
+router.get("/plato/:id", menuController.getPlatoById);
+
 // Ruta para crear menus
 router.post("/create", menuController.createMenu);
 
-// Ruta para actualizar 
+// Ruta para crear plato
+router.post("/createPlato", menuController.createPlato);
+
+// Ruta para crear bebida
+router.post("/createBebida", menuController.createBebida);
+
+// Ruta para actualizar menu
 router.put("/:id", menuController.updateMenu);
+
+// Ruta para actualizar plato
+router.put("/plato/:id", menuController.updatePlato);
+
+// Ruta para actualizar bebida
+router.put("/bebida/:id", menuController.updateBebida);
 
 // Ruta para eliminar menu
 router.delete("/:id", menuController.deleteMenu);
+
+// Ruta para eliminar Bebida
+router.delete("/bebida/:id", menuController.deleteBebida);
+
+// Ruta para eliminar Bebida
+router.delete("/plato/:id", menuController.deletePlato);
 
 // 5- Exportamos el modulo
 module.exports = router;
